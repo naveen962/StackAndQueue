a060be4719e6fcab5b2642d593916761aeba7a18
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-10-06 05:11:49 -0700

    peak and pop operation is added

---
+++ LinkedList_Stack_UC2/LinkedList_Stack_UC2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LinkedList_Stack_UC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine("Welcome to LINKEDLIST-STACK");
+            Stack linkedListStack = new Stack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
+            linkedListStack.Peek();
+            linkedListStack.Pop();
+            linkedListStack.IsEmpty();
+        }
+    }
+}
---
+++ LinkedList_Stack_UC2/LinkedList_Stack_UC2/Stack.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList_Stack_UC2
+{
+    class Stack
+    {
+
+        private Node top;
+        public Stack()
+        {
+            this.top = null;
+        }
+        /// <summary>
+        /// create push method
+        /// </summary>
+        /// <param name="value"></param>
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = top;
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack ", value);
+        }
+        /// <summary>
+        /// create display method
+        /// </summary>
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        /// <summary>
+        /// adding peek method
+        /// </summary>
+        internal void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("\n{0} is in the top of the stack ", this.top.data);
+        }
+        /// <summary>
+        /// adding pop method
+        /// </summary>
+        internal void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty, Deletion is not possible");
+                return;
+            }
+            Console.WriteLine("Value popped is {0} ", this.top.data);
+            this.top = this.top.next;
+        }
+        internal void IsEmpty()
+        {
+            while (this.top != null)
+            {
+                Peek();
+                Pop();
+            }
+        }
+
+    }
+}
