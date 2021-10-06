ec1376481180a864a3d8e35f765aa5d7668dec70
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-10-06 05:18:05 -0700

    program to perform dequeue using linkedlist is added

---
+++ QueueUC4/QueueUC4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace QueueUC4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+           Queue linkedListQueue = new Queue();
+            linkedListQueue.Enqueue(70);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Dequeue();
+            Console.WriteLine("After performing one dequeue operation the queue is having  : ");
+            linkedListQueue.Display();
+        }
+    }
+}
---
+++ QueueUC4/QueueUC4/Queue.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueueUC4
+{
+    class Queue
+    {
+        Node head = null;
+        /// <summary>
+        /// create method to add data
+        /// </summary>
+        /// <param name="data"></param>
+        internal void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+                head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into queue ", node.data);
+        }
+
+        /// <summary>
+        /// create method to display
+        /// </summary>
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        /// <summary>
+        /// create dequeue method
+        /// </summary>
+        /// <returns></returns>
+        internal Node Dequeue()
+        {
+            if (this.head == null)
+                return null;
+            this.head = this.head.next;
+            return this.head;
+        }
+    }
+}
