93620c41c8d8f70c2e856ea203db33ec5de179a0
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-10-06 05:08:04 -0700

    adding push and display

---
+++ LinkedList_Stack/LinkedList_Stack/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LinkedList_Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LINKEDLIST-STACK");
+            Stack linkedListStack = new Stack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
+        }
+    }
+    
+}
---
+++ LinkedList_Stack/LinkedList_Stack/Stack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList_Stack
+{
+    class Stack
+    {
+        private Node top;
+        public Stack()
+        {
+            this.top = null;
+        }
+        /// <summary>
+        /// create push method
+        /// </summary>
+        /// <param name="value"></param>
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack ", value);
+        }
+        /// <summary>
+        /// create display method
+        /// </summary>
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
+
