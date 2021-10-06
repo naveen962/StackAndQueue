b688617ae27aba0ae89a2e822f799e41a180554c
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-10-06 05:15:39 -0700

    program to perform enqueue and display using linkedlist is added

---
+++ QueueUC3/QueueUC3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace QueueUC3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Queue linkedListQueue = new Queue();
+            linkedListQueue.Enqueue(70);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Display();
+        }
+    }
+}
